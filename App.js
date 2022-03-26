776bc585c10f3920b1424ad8729406d1e0d016f9
AllenZZP <131452abc@gmail.com>
2022-03-26 00:46:01 -0700

    implement swipeable and delete feature

--- App.js
+++ App.js
@@ -6,11 +6,12 @@ import WelcomeScreen from "./app/screens/WelcomeScreen";
 import AppButton from "./app/components/AppButton";
 import Card from "./app/components/Card";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
+import MessagesScreen from "./app/screens/MessagesScreen";
 
 
 export default function App() {
   return (
-    <ViewImageScreen />
+    <MessagesScreen />
   );
 }
 
--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -1,25 +1,34 @@
 import React from 'react';
-import { Image, StyleSheet, View } from 'react-native';
+import { Image, StyleSheet, View, TouchableHighlight } from 'react-native';
 import AppText from './AppText';
+import Swipeable from "react-native-gesture-handler/Swipeable";
 
 import colors from '../config/colors';
 
 function ListItem(props) {
-  const {title, subTitle, image} = props;
+  const { title, subTitle, image, onPress, renderRightActions } = props;
   return (
-    <View style={ styles.container }>
-      <Image style={ styles.image } source={ image } />
-      <View>
-        <AppText style={ styles.title }>{ title }</AppText>
-        <AppText style={ styles.subTitle }>{ subTitle }</AppText>
-      </View>
-    </View>
+    <Swipeable renderRightActions={ renderRightActions }>
+      <TouchableHighlight
+        underlayColor={ colors.light }
+        onPress={ onPress }
+      >
+        <View style={ styles.container }>
+          <Image style={ styles.image } source={ image } />
+          <View>
+            <AppText style={ styles.title }>{ title }</AppText>
+            <AppText style={ styles.subTitle }>{ subTitle }</AppText>
+          </View>
+        </View>
+      </TouchableHighlight>
+    </Swipeable>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: "row"
+    flexDirection: "row",
+    padding: 15
   },
   image: {
     borderRadius: 35,
