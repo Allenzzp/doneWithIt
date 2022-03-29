69e4ee876f8eae55135f2926340e3c439c82dda5
AllenZZP <131452abc@gmail.com>
2022-03-28 18:00:05 -0700

    abstract default style

--- app/components/AppText.js
+++ app/components/AppText.js
@@ -1,28 +1,14 @@
 import React from 'react';
-import { Text, StyleSheet, Platform } from "react-native";
+import { Text } from "react-native";
+import defaultStyles from "../config/styles";
 
 //encapsulate styles in a custom component
 function AppText(props) {
   const { children, style } = props;
   return (
-    <Text style={ [styles.text, style] }>{ children }</Text>
+    <Text style={ [defaultStyles.text, style] }>{ children }</Text>
   );
 }
 
-const styles = StyleSheet.create({
-  text: {
-    ...Platform.select({
-      ios: {
-        fontSize: 20,
-        fontFamily: "Avenir",
-      },
-      android: {
-        fontSize: 18,
-        fontFamily: "Roboto"
-      }
-    }),
-  
-  }
-})
 
 export default AppText;
