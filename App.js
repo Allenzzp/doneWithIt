d90e9c7c282a5aa7e878f027f536beae1746a0fc
AllenZZP <131452abc@gmail.com>
2022-03-28 17:48:03 -0700

    implemented TextInput

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { View } from 'react-native'
+import React, { useState } from "react";
+import { View, TextInput, Text } from 'react-native'
 
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
@@ -12,11 +12,23 @@ import Icon from "./app/components/Icon";
 import ListItem from "./app/components/ListItem";
 import AccountScreen from "./app/screens/AccountScreen";
 import ListingsScreen from "./app/screens/ListingsScreen";
+import AppTextInput from "./app/components/AppTextInput";
 
 
 export default function App() {
+  const [firstName, setFirstName] = useState("");
   return (
-    <ListingsScreen />
+    // <WelcomeScreen />
+    // <ListingsScreen />
+    // <ListingDetailsScreen />
+    // <MessagesScreen />
+    // <AccountScreen />
+    <Screen>
+      <AppTextInput 
+        placeholder="Username"
+        icon="email"
+      />
+    </Screen>
   );
 }
 
